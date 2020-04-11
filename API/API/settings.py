fc9f6187a28b7354bcead96c1b4dfe477eafb15d
Saya <601470429@qq.com>
2020-04-11 13:04:11 +0800

    changes for rename

--- API/API/settings.py
+++ API/API/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'snippets',
+    'API_interface',
 ]
 
 MIDDLEWARE = [
--- API/API/urls.py
+++ API/API/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('timetable/', include('snippets.urls')),
+    path('timetable/', include('API_interface.urls')),
 ]

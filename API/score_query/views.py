b327ff9b426b218ac7cd64225a5a6a08c60efcea
Saya <601470429@qq.com>
2020-04-13 16:25:13 +0800

    add help

--- API/score_query/views.py
+++ API/score_query/views.py
@@ -6,6 +6,11 @@ from .models import *
 
 
 class ScoreList(APIView):
+    # 参数1:学生学号 e.g. 17373333
+    # 参数2：学期 e.g.2020_Spring
+    # 参数类型错误、不给参数均返回404错误
+    # 例:127.0.0.1/score?student_id=11111111&semester=2020_Spring
+    # 获得学号为11111111，2020春季学期的成绩
     @staticmethod
     def get(request):
         req = request.query_params.dict()

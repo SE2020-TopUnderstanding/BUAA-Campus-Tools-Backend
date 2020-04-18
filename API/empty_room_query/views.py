3b25d6139e055fed11abe39eb84e980d6be70586
胡彬彬 <17373349@buaa.edu.cn>
2020-04-18 09:07:24 +0800

    按照前端要求修改返回格式，并按照pm要求写注释，接口文档修改了

--- API/empty_room_query/views.py
+++ API/empty_room_query/views.py
@@ -5,40 +5,18 @@ from course_query.models import Student
 from .models import *
 
 
-#campus 校区
-#teaching_building 教学楼
-#classroom 教室
-#date 日期
-#section 节数
-'''
-查询方法
-http http://127.0.0.1:8000/query/classroom/ campus="学院路校区" date="2020-04-13" section="1,2,"
-返回格式
-{
-    "一号楼": [
-        {
-            "classroom": "(一)103"
-        },
-        {
-            "classroom": "(一)104"
-        },
-        {
-            "classroom": "(一)105"
-        }
-    ],
-    "二号楼": [
-        {
-            "classroom": "(一)105"
-        }
-    ]
-}
-'''
-class query_classroom(APIView):#输入：校区，日期，第几节到第几节 返回：教学楼，教室
-    def get(self, request, format=None):
-        content = {"state":"未定义"}
-        return Response(content)
 
-    def post(self, request, format=None):#
+
+class query_classroom(APIView):
+    def get(self, request, format=None):
+        """
+        输入：校区，日期，第几节到第几节，返回：教学楼，教室
+        参数1:校区 e.g. 新主楼
+        参数2：日期 e.g. 2020-4-18
+        参数3：第几节到第几节 e.g. 1,2,3,
+        例:http --form GET http://127.0.0.1:8000/classroom/ campus="新主楼" date="2020-4-18" section="1,2,3,"
+        返回:登录状态
+        """
         campus = request.data["campus"]
         date = request.data["date"]
         section = request.data["section"]
@@ -52,65 +30,8 @@ class query_classroom(APIView):#输入：校区，日期，第几节到第几节
             cr_re = re.filter(teaching_building=i["teaching_building"]).values("classroom").distinct()
             content.update({i["teaching_building"]:cr_re})
 
-
         return Response(content)
 
-
-
-#objects
-#course
-#homework
-#dll
-#state
-#student_id 
-'''
-返回格式
-{
-    "计网": [
-        {
-            "dll": "2010-10-11",
-            "homework": "团队作业",
-            "state": "已提交"
-        }
-    ],
-    "软工": [
-        {
-            "dll": "2010-10-9",
-            "homework": "团队作业",
-            "state": "已提交"
-        },
-        {
-            "dll": "2010-10-10",
-            "homework": "个人作业",
-            "state": "已提交"
-        },
-        {
-            "dll": "2010-10-12",
-            "homework": "最后一次作业",
-            "state": "未提交"
-        },
-        {
-            "dll": "2010-10-13",
-            "homework": "团队作业",
-            "state": "未提交"
-        }
-    ]
-}
-'''
-class query_ddl(APIView):#输入学号：输出作业，dll，提交状态，课程
-    def get(self, request, format=None):
+    def post(self, request, format=None):#
         content = {"state":"未定义"}
         return Response(content)
-    def post(self, request, format=None):#
-        student_id = request.data["student_id"]
-        content = {}
-        re = DDL_t.objects.filter(student_id=student_id)
-
-        course_re = re.values("course").distinct()
-
-        for i in course_re:
-            cr_re = re.filter(course=i["course"]).values("homework","dll", "state").distinct()
-            content.update({i["course"]:cr_re})
-        return Response(content)
-
-        
--- API/user_login/views.py
+++ API/user_login/views.py
@@ -5,10 +5,12 @@ from course_query.models import Student
 
 class login(APIView):
     def get(self, request, format=None):
-        content = {"state":"未定义"}
-        return Response(content)
-
-    def post(self, request, format=None):
+        """
+        输入：用户名，用户密码，输出：登录状态（1代表成功，2代表无该账号，3代表密码错误）
+        参数1:用户名 e.g. mushan，用户密码 e.g. h1010
+        例:http --form GET http://127.0.0.1:8000/login/ usr_name="mushan" usr_password="h1010"
+        返回:登录状态
+        """
         usr_name = request.data["usr_name"]
         usr_password = request.data["usr_password"]
         #调用爬虫接口进行验证并取得下面的值
@@ -20,4 +22,8 @@ class login(APIView):
 
         Student(id="1").save()
         print(Student.objects.filter(id="1").values("id"))
+        return Response(content)
+
+    def post(self, request, format=None):
+        content = {"state":"未定义"}
         return Response(content)

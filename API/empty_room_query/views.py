1a9311aae304ce6f3b4c8587d0be1b49156f9fe2
胡彬彬 <17373349@buaa.edu.cn>
2020-04-19 14:06:29 +0800

    后端与爬虫结合接口完成

--- API/empty_room_query/views.py
+++ API/empty_room_query/views.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.http import Http404
 from course_query.models import Student
+from django.http import Http404, HttpResponseBadRequest, HttpResponse
 from .models import *
 
 
@@ -14,7 +15,7 @@ class query_classroom(APIView):
         参数1:校区 e.g. 新主楼
         参数2：日期 e.g. 2020-4-18
         参数3：第几节到第几节 e.g. 1,2,3,
-        例:http://127.0.0.1:8000/classroom/?campus=新主楼&date=2020-4-18&section=1,2,3,
+        例:http://127.0.0.1:8000/classroom/?campus=学院路校区&date=2020-4-18&section=1,2,3,
         返回:登录状态
         """
         req = request.query_params.dict()
@@ -34,5 +35,49 @@ class query_classroom(APIView):
         return Response(content)
 
     def post(self, request, format=None):#
-        content = {"state":"未定义"}
+        '''
+            [
+                {
+                    "campus":"学院路校区",
+                    "content":[
+                        {
+                            "teaching_building":"一号楼",
+                            "classroom":"(一)203",
+                            "date":"2020-04-20",
+                            "section":"1,2,3,4,5,7,"
+                        },
+                        {
+                            "teaching_building":"一号楼",
+                            "classroom":"(一)204",
+                            "date":"2020-04-20",
+                            "section":"1,2,3,4,7,"
+                        }
+                    ]
+                },
+                {
+                    "campus":"沙河校区",
+                    "content":[
+                        {
+                            "teaching_building":"三号楼",
+                            "classroom":"(三)202",
+                            "date":"2020-04-20",
+                            "section":"3,4,5,7,8,"
+                        }
+                    ]
+                }
+            ]
+        '''
+        req = request.data
+        Classroom_t.objects.all().delete()
+        for key in req:
+            if len(key) == 2:
+                campus = key["campus"]
+                content = key["content"]
+                for i in content:
+                    Classroom_t(campus=campus,teaching_building=i["teaching_building"],
+                        classroom=i["classroom"],date=i["date"],section=i["section"]).save()
+            else:
+                return HttpResponseBadRequest()
+
+        content = {"state":1}
         return Response(content)

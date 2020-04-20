3ed12c75859af4fe3e8632c3fdcfb6896ea3dbbd
Saya <601470429@qq.com>
2020-04-20 19:46:06 +0800

    细节改动

--- API/course_query/views.py
+++ API/course_query/views.py
@@ -37,9 +37,9 @@ class CourseList(APIView):
     def get(request):
         """可使用的参数有：
         student_id: 附带学生学号，查询指定学生课表;
-        semester: 附带学年学期，查询指定学期课表;
-        week: 附带周数，查询指定周的课表，若参数值为all,则查询该学期全部周的课表;
-        例：127.0.0.1/timetable?student_id=17333333&semester=2020_Spring&week=3
+        week: 附带周数，查询指定周的课表;
+        date: 返回这周是第几周
+        例：127.0.0.1/timetable?student_id=17333333&week=3
         查询学号为17373333 2020春季学期第三周课表;
         没有提供参数，参数数量错误，返回400错误;
         参数错误，返回404错误;
@@ -47,6 +47,7 @@ class CourseList(APIView):
         start_day = '2020-2-24'
         req = request.query_params.dict()
         result = StudentCourse.objects.all()
+        # 课表查询请求
         if len(req) == 2:
             for key, value in req.items():
                 if key == 'student_id':
@@ -58,6 +59,7 @@ class CourseList(APIView):
                     raise Http404
             course_serializer = StudentCourseSerializer(result, many=True)
             return Response(course_serializer.data)
+        # 当前周查询请求
         elif len(req) == 1:
             if 'date' in req.keys():
                 content = []

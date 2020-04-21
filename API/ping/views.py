12ffa07301933e30cf389a48beeeb5f6ac51303f
sharinka <sharinka0715@gmail.com>
2020-04-21 23:42:06 +0800

    fixed time

--- API/ping/views.py
+++ API/ping/views.py
@@ -1,13 +1,11 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-last_updated = ""
-
 class Ping(APIView):
     @staticmethod
     def get(request):
         return Response(status=200, data={
             "后端更新时间": "",
             "后端所在PR": "",
-            "爬虫上一次活跃时间": last_updated
+            "爬虫上一次活跃时间": 
         })
--- API/request_queue/models.py
+++ API/request_queue/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class TimeInfo(models.Model):
+    time = models.DateTimeField(auto_now=True)

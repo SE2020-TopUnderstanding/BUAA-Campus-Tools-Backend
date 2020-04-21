74c016facd84aa1ee9f2c70d7e911dde90be78c4
sharinka <sharinka0715@gmail.com>
2020-04-21 23:45:19 +0800

    fixed time

--- API/ping/views.py
+++ API/ping/views.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from request_queue.models import TimeInfo
 
 class Ping(APIView):
     @staticmethod
@@ -7,5 +8,5 @@ class Ping(APIView):
         return Response(status=200, data={
             "后端更新时间": "",
             "后端所在PR": "",
-            "爬虫上一次活跃时间": 
+            "爬虫上一次活跃时间": TimeInfo.objects.get(id=1)
         })

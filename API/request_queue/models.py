8af6fbd48b7446bfb34d0369553c282e865c75f8
Saya <601470429@qq.com>
2020-04-23 14:41:52 +0800

    添加记录统计

--- API/request_queue/models.py
+++ API/request_queue/models.py
@@ -7,5 +7,5 @@ class TimeInfo(models.Model):
 
 
 class RequestRecord(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, primary_key=True)
     count = models.IntegerField(default=0)

39c2c2d9e04605f25db42770fa2fb9208f9e7f48
Saya <601470429@qq.com>
2020-04-23 14:40:50 +0800

    添加记录统计

--- API/request_queue/models.py
+++ API/request_queue/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
 class TimeInfo(models.Model):
     time = models.DateTimeField(auto_now=True)
+
+
+class RequestRecord(models.Model):
+    name = models.CharField(max_length=50)
+    count = models.IntegerField(default=0)

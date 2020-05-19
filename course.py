3ae2ff3c3fe7aa3ec0722fb56f602ca4a86f009c
杜博玮 <17373010@buaa.edu.cn>
2020-05-19 19:16:43 +0800

    重构课程中心爬虫

--- course.py
+++ course.py
@@ -1,8 +1,11 @@
 import time
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as Ec
-from selenium.webdriver.common.by import By
-from vpn import VpnLogin
+import requests
+from bs4 import BeautifulSoup
+from web import WebLogin
+
+
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) ' \
+             'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36'
 
 
 class CourseRequest:
@@ -10,48 +13,73 @@ class CourseRequest:
     本类将从 course.buaa.edu.cn 网站获取信息
     """
     def __init__(self, user_name, pw):
-        self.status = 0
-        vpn = ''
+        # 登陆用的网页地址、user_agent、header等
+        self.course_url = 'https://course.e2.buaa.edu.cn/portal'
+        self.headers_course = {
+            'Referer': 'https://course.e2.buaa.edu.cn/portal',
+            'User-Agent': USER_AGENT
+        }
+
+        self.status = 1
+        self.now = ''                           # 整个爬取过程的session
+        login = ''
         for i in range(3):
-            vpn = VpnLogin(user_name, pw)  # 登录
-            success = vpn.switch_to_course()      # 切换到课程中心
+            login = WebLogin(user_name, pw)     # 登录
+            success = login.switch_to_course()  # 切换到课程中心
             # 错误处理
-            if success == -1:
-                self.status = -1
-                break
-            if success in (-2, -5):
+            if success == -4:
                 if i == 2:
-                    self.status = -2
+                    self.status = -4
                     break
-                vpn.get_browser().quit()
-            if success in (-3, -4):
-                self.status = -3
+            elif success == 1:
+                self.now = login.get_session()
                 break
-            if -6 >= success >= -10:
-                self.status = success + 1
+            else:
+                self.status = success
                 break
-            if success == 0:
-                break
-        if vpn == '':
+        if login == '':
             self.status = -11
             return
-        self.browser = vpn.get_browser()         # 获取浏览器.
-        # self.getDdl()                           # debug用
+
+        # self.get_ddl()                        # debug用
+        # self.get_id()                         # debug用
 
     def get_status(self):
         """
         初始化的结果
-        status =  0 : 成功
-        status = -1 : 登陆错误
-        status = -2 : 超时3次
-        status = -3 : 未知错误
-        status = -5 : IP被封
-        status = -6 : 用户名错误或者网站要求输入验证码
-        status = -7 : 密码错误
-        status = -8 : 用户名或密码为空
-        status = -9 : 账号被锁
+        status =  1 : 成功
+        status =  0 : 登录课程中心出现未知错误，通常是超时问题
+        status = -1 : 登陆时出现未知错误，请参考log信息
+        status = -2 : 登录状态码是2XX，但不是200
+        status = -3 : 跳转到未知网页
+        status = -4 : 超时3次
+        status = -5 : 登陆或访问教务状态码是4XX或5XX
+        status = -6 : IP被封
+        status = -7 : 用户名错误或者网站要求输入验证码
+        status = -8 : 密码错误
+        status = -9 : 用户名或密码为空
+        status =-10 : 账号被锁
+        status =-11 : 账号被锁
+        status =-12 : 在课程中心请求失败
         """
         return self.status
+
+    @staticmethod
+    def check_status(html):
+        """
+        用于检查登录是否成功
+        成功返回  0
+        失败返回 -1
+        """
+        try:
+            if html != '':
+                html.raise_for_status()
+            else:
+                return -1
+        except requests.exceptions.RequestException as err:
+            print(err)
+            return -1
+        return 0
 
     def get_ddl(self):
         """
@@ -64,143 +92,211 @@ class CourseRequest:
             ...
         }
         """
-        if self.status != 0:
+        if self.status != 1:
             return self.status
-        # noinspection PyBroadException
-        try:
-            cur_lessons = self.browser.find_element_by_xpath('//*[@id="otherSitesCategorWrap"]/ul[1]')
-        except Exception:
-            print('timeout or this guy redefined the network')
-            time.sleep(5)
-            # noinspection PyBroadException
+        page = ''
+        i = 0
+        while i < 3:
             try:
-                cur_lessons = self.browser.find_element_by_xpath('//*[@id="otherSitesCategorWrap"]/ul[1]')
-            except Exception:
-                print('this guy redefined the network')
-                return -10
-        lessons = cur_lessons.find_elements_by_xpath('li')                                       # 获取所有课程
-        ddls = {}
-        for i in range(len(lessons)):
-            link = lessons[i].find_elements_by_xpath('a')[0]
-            # print(link.get_attribute('href'))
-            cur_lesson = link.get_attribute('title')                                             # 获取课程名字
-            # print(curLesson)
-            link = link.get_attribute('href')
+                page = self.now.get(url=self.course_url, headers=self.headers_course)  # 先获取完整网页
+                break
+            except requests.exceptions.RequestException as err:
+                print(err)
+                i += 1
+                if i == 3:
+                    return 0
 
-            self.browser.get(link)                                                              # 切换到该课程页面
-            locator = (By.XPATH, '//*[@id="toolMenu"]/ul')
-            # noinspection PyBroadException
-            try:
-                WebDriverWait(self.browser, 5).until(Ec.presence_of_element_located(locator))
-            except Exception:
-                print('timeout or switch to an unknown page')
-                return -4
-            work_buttons = self.browser.find_element_by_xpath('//*[@id="toolMenu"]/ul')
-            work_buttons = work_buttons.find_elements_by_tag_name('a')
-            work_button = 0
-            for each in work_buttons:
-                if each.get_attribute('title') == '在线发布、提交和批改作业':
-                    work_button = each
+        if self.check_status(page) == -1:
+            return -12
+
+        soup = BeautifulSoup(page.text, 'lxml')
+        table = soup.select('div #otherSitesCategorWrap > ul')                         # 获取所有课程的名称和网址
+        lessons = {}
+        for each in table[0].contents:
+            if each == '\n':
+                continue
+            lessons[each.contents[1].attrs['title']] = each.contents[1].attrs['href']
+
+        ddls = {}
+
+        for lesson_title, url in lessons.items():
+            this_ddls = []
+            lesson = ''
+            i = 0
+            while i < 3:
+                try:
+                    lesson = self.now.get(url=url, headers=self.headers_course)         # 访问该课程的主页
                     break
-            if work_button == 0:
-                self.browser.switch_to.default_content()                                            # 重新获取下一个课程
-                cur_lessons = self.browser.find_element_by_xpath('//*[@id="otherSitesCategorWrap"]/ul[1]')
-                lessons = cur_lessons.find_elements_by_xpath('li')
+                except requests.exceptions.RequestException as err:
+                    print(err)
+                    i += 1
+                    if i == 3:
+                        return 0
+
+            if self.check_status(lesson) == -1:
+                return -12
+
+            soup = BeautifulSoup(lesson.text, 'lxml')
+            table = soup.select('a[class="toolMenuLink"]')                              # 获取该课程的作业的网址
+            info_url = ''
+            for each in table:
+                # print(each.attrs['title'])
+                if each.attrs['title'].find('在线发布、提交和批改作业') != -1:
+                    info_url = each.attrs['href']
+                    break
+            if info_url == '':
                 continue
 
-            work_button.click()
-            frame = self.browser.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[3]/div/div/div[2]/iframe')
-            self.browser.switch_to.frame(frame)                                                 # 切换到另一个frame获取数据
-            locator = (By.XPATH, '/html/body/div')
-            # noinspection PyBroadException
-            try:
-                WebDriverWait(self.browser, 5).until(Ec.presence_of_element_located(locator))
-            except Exception:
-                print('timeout or switch to an unknown page')
-                return -4
-            # noinspection PyBroadException
-            try:
-                self.browser.find_element_by_xpath('/html/body/div/form/table')                 # 检查是否存在作业
-            except Exception:
-                # noinspection PyBroadException
+            i = 0
+            while i < 3:
                 try:
-                    self.browser.find_element_by_xpath('/html/body/div/p')
-                    # print('no homework founded\n')
-                    ddls[cur_lesson] = []
+                    lesson = self.now.get(url=info_url, headers=self.headers_course)    # 访问作业部分
+                    break
+                except requests.exceptions.RequestException as err:
+                    print(err)
+                    i += 1
+                    if i == 3:
+                        return 0
 
-                    self.browser.switch_to.default_content()
-                    time.sleep(0.5)
-                    cur_lessons = self.browser.find_element_by_xpath('//*[@id="otherSitesCategorWrap"]/ul[1]')
-                    lessons = cur_lessons.find_elements_by_xpath('li')                           # 如果没有作业
+            if self.check_status(lesson) == -1:
+                return -12
 
-                    continue
-                except Exception:
-                    print('timeout or switch to an unknown page')
-                    return -4
-            table = self.browser.find_element_by_xpath('/html/body/div/form/table')
-            table_rows = table.find_elements_by_tag_name('tr')[1:]
-            jobs = []
-            for row in table_rows:                                                               # 获取作业信息
-                datas = row.find_elements_by_tag_name('td')[1:]
-                work = []
-                if len(datas) >= 1:
-                    title = datas[0].find_elements_by_tag_name('a')
-                    if title:
-                        title = title[0]
-                    else:
-                        title = datas[0].find_elements_by_tag_name('span')[0]
-                    title = title.text
-                    # print(title)
-                    work.append(title)
-                if len(datas) >= 2:
-                    status = datas[1].text
-                    # print(status)
-                    work.append(status)
-                if len(datas) >= 3:
-                    opendate = datas[2].text
-                    # print(opendate)
-                    work.append(opendate)
-                if len(datas) >= 4:
-                    duedate = datas[3].find_elements_by_tag_name('span')
-                    if duedate:
-                        duedate = duedate[0].text
-                    else:
-                        duedate = datas[3].text
-                    # print(duedate)
-                    work.append(duedate)
-                while len(work) < 4:
-                    work.append('')
-                # print()
-                jobs.append(work)
-            ddls[cur_lesson] = jobs
+            soup = BeautifulSoup(lesson.text, 'lxml')
+            table = soup.select('div[class="portletMainWrap"] > iframe')                # 获取iframe的网址
+            iframe_url = ''
+            for each in table:
+                iframe_url = each.attrs['src']
+                break
 
-            self.browser.switch_to.default_content()                                            # 重新获取下一个课程
-            time.sleep(0.5)
-            cur_lessons = self.browser.find_element_by_xpath('//*[@id="otherSitesCategorWrap"]/ul[1]')
-            lessons = cur_lessons.find_elements_by_xpath('li')
-        return ddls
+            if iframe_url == '':
+                return -12
+
+            i = 0
+            while i < 3:
+                try:
+                    lesson = self.now.get(url=iframe_url, headers=self.headers_course)  # 访问iframe
+                    break
+                except requests.exceptions.RequestException as err:
+                    print(err)
+                    i += 1
+                    if i == 3:
+                        return 0
+
+            if self.check_status(lesson) == -1:
+                return -12
+
+            soup = BeautifulSoup(lesson.text, 'lxml')
+            table = soup.select('table[class="listHier lines nolines"] > tr')           # 获取所有作业的信息
+            for each in table[1:]:
+                # print(each.get_text())
+                title = each.contents[3].get_text().replace('  ', '')
+                title = title.replace('\n', '')
+                title = title.replace('\t', '')
+                state = each.contents[5].get_text().replace('  ', '')
+                state = state.replace('\n', '')
+                state = state.replace('\t', '')
+                begin = each.contents[7].get_text().replace('  ', '')
+                begin = begin.replace('\n', '')
+                begin = begin.replace('\t', '')
+                end = each.contents[9].get_text().replace('  ', '')
+                end = end.replace('\n', '')
+                end = end.replace('\t', '')
+                work_ddls = [title, state, begin, end]
+                this_ddls.append(work_ddls)
+            ddls[lesson_title] = this_ddls
+            time.sleep(1)
+        print(ddls)
+        return ''
 
     def get_id(self):
-        if self.status != 0:
+        """
+        该函数返回该学生的学号
+        """
+        if self.status != 1:
             return self.status
-        # noinspection PyBroadException
-        try:
-            name_button = self.browser.find_element_by_xpath('//*[@id="toolMenu"]/ul/li[9]/a')
-        except Exception:
-            print('timeout or switch to an unknown page')
-            return -4
-        name_button.click()
-        frame = self.browser.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[3]/div/div/div[2]/iframe')
-        self.browser.switch_to.frame(frame)                                                 # 切换到另一个frame获取数据
-        time.sleep(0.5)
-        stu_id = self.browser.find_element_by_xpath('//*[@id="userViewForm"]/fieldset/div[1]')
-        stu_id = stu_id.text
-        self.browser.switch_to.default_content()
-        time.sleep(0.5)
-        return stu_id.split(' ')[1]
+        page = ''
+        i = 0
+        while i < 3:
+            try:
+                page = self.now.get(url=self.course_url, headers=self.headers_course)   # 先获取完整网页
+                break
+            except requests.exceptions.RequestException as err:
+                print(err)
+                i += 1
+                if i == 3:
+                    return 0
+
+        if self.check_status(page) == -1:
+            return -12
+
+        soup = BeautifulSoup(page.text, 'lxml')
+        table = soup.select('a[class="toolMenuLink"]')                                  # 获取个人信息的网址
+        info_url = ''
+        for each in table:
+            print(each.attrs['title'])
+            if each.attrs['title'].find('查看和修改我的个人信息') != -1:
+                info_url = each.attrs['href']
+                break
+
+        if info_url == '':
+            return -12
+        i = 0
+        while i < 3:
+            try:
+                page = self.now.get(url=info_url, headers=self.headers_course)          # 访问个人信息网址
+                break
+            except requests.exceptions.RequestException as err:
+                print(err)
+                i += 1
+                if i == 3:
+                    return 0
+
+        if self.check_status(page) == -1:
+            return -12
+
+        soup = BeautifulSoup(page.text, 'lxml')
+        table = soup.select('div[class="portletMainWrap"] > iframe')                    # 获取iframe的网址
+        iframe_url = ''
+        for each in table:
+            iframe_url = each.attrs['src']
+            break
+
+        if iframe_url == '':
+            return -12
+        i = 0
+        while i < 3:
+            try:
+                page = self.now.get(url=iframe_url, headers=self.headers_course)        # 访问iframe
+                break
+            except requests.exceptions.RequestException as err:
+                print(err)
+                i += 1
+                if i == 3:
+                    return 0
+
+        if self.check_status(page) == -1:
+            return -12
+
+        soup = BeautifulSoup(page.text, 'lxml')
+        table = soup.select('div[class="shorttext"]')                                   # 获取学号
+        stu_id = ''
+        for each in table:
+            print(each.get_text())
+            strs = each.get_text()
+            label = strs.split('\n')[1]
+            stu_id = strs.split('\n')[2].replace(' ', '')
+            if label == '用户ID':
+                break
+            stu_id = ''
+        if stu_id == '':
+            return -12
+        return stu_id
 
     def quit(self):
-        self.browser.quit()
+        """
+        该函数结束整个session访问过程
+        """
+        self.now.close()
 
 
 # 测试用

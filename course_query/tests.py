317ad82a73a11678182d9c61f2c33c953e841420
Saya <601470429@qq.com>
2020-05-23 19:34:55 +0800

    完成基本单元测试

--- course_query/tests.py
+++ course_query/tests.py
@@ -193,3 +193,190 @@ class CourseGetTest(TestCase):
         evaluation.save()
         actor, return_evaluation = get_evaluation({'student_id': '17373456', 'bid': '111', 'actor': '17373010'})
         self.assertEqual(return_evaluation, evaluation)
+
+    def test_up_action(self):
+        actor = Student(id='17373010', usr_name='111')
+        actor.save()
+        student = Student(id='17373456')
+        student.save()
+        course = Course(bid='111')
+        course.save()
+        evaluation = CourseEvaluation(course=course, student=student)
+        evaluation.save()
+        result = up_action(evaluation, actor)
+        self.assertEqual(result.status_code, 201)
+
+    def test_down_action(self):
+        actor = Student(id='17373010', usr_name='111')
+        actor.save()
+        student = Student(id='17373456')
+        student.save()
+        course = Course(bid='111')
+        course.save()
+        evaluation = CourseEvaluation(course=course, student=student)
+        evaluation.save()
+        result = down_action(evaluation, actor)
+        self.assertEqual(result.status_code, 201)
+
+    def test_search_get_success(self):
+        client = Client()
+        course = Course(bid='111', name='计算机网络', credit='2.0', hours='32', department='计算机学院', type='核心专业类')
+        course.save()
+        teacher = Teacher(name='张辉')
+        teacher.save()
+        add_teacher_relation(teacher, course)
+        response = client.get("/timetable/search/?course=计&teacher=张&type=核心专业类&department=计算机学院")
+        self.assertEqual(response.status_code, 200)
+
+    def test_search_get_fail(self):
+        client = Client()
+        response = client.get("/timetable/search/?teach=计")
+        self.assertEqual(response.status_code, 400)
+
+    def test_search_default(self):
+        client = Client()
+        student = Student(id='17373010')
+        student.save()
+        course = Course(bid='111', name='计算机网络', credit='2.0', hours='32', department='计算机学院', type='核心专业类')
+        course.save()
+        teacher = Teacher(name='张辉')
+        teacher.save()
+        add_teacher_relation(teacher, course)
+        infos = ['111', '(一)305', '荣文戈， 张辉', '1-16', '周1 第3，4节']
+        add_student_course(student=student, semester='2020_Spring', info=infos)
+        response = client.get('/timetable/search/default/?student_id=17373010')
+        self.assertEqual(response.status_code, 200)
+
+    def test_search_default_fail_401(self):
+        client = Client()
+        response = client.get('/timetable/search/default/?student_id=17373010')
+        self.assertEqual(response.status_code, 401)
+
+    def test_search_default_fail_400(self):
+        client = Client()
+        response = client.get('/timetable/search/default/?student=17373010')
+        self.assertEqual(response.status_code, 400)
+
+    def test_course_evaluations_get(self):
+        client = Client()
+        student = Student(id='17373010', usr_name='111')
+        student.save()
+        student = Student(id='17373456')
+        student.save()
+        course = Course(bid='111')
+        course.save()
+        evaluation = CourseEvaluation(course=course, student=student)
+        evaluation.save()
+        response = client.get('/timetable/evaluation/student/?bid=111&student_id=17373010')
+        self.assertEqual(response.status_code, 200)
+
+    def test_course_evaluations_put(self):
+        client = Client()
+        student = Student(id='17373010')
+        student.save()
+        course = Course(bid='111')
+        course.save()
+        data = {"bid": "111", "text": "233", "score": 3, "student_id": "17373010"}
+        response = client.put("/timetable/evaluation/student/", content_type='application/json', data=data)
+        self.assertEqual(response.status_code, 201)
+
+    def test_course_evaluations_delete(self):
+        client = Client()
+        student = Student(id='17373456')
+        student.save()
+        course = Course(bid='111')
+        course.save()
+        evaluation = CourseEvaluation(course=course, student=student)
+        evaluation.save()
+        data = {"student_id": "17373456", "bid": "111"}
+        response = client.delete("/timetable/evaluation/student/", content_type='application/json', data=data)
+        self.assertEqual(response.status_code, 204)
+
+    def test_evaluation_up_action(self):
+        client = Client()
+        student = Student(id='17373010', usr_name='111')
+        student.save()
+        student = Student(id='17373456')
+        student.save()
+        course = Course(bid='111')
+        course.save()
+        evaluation = CourseEvaluation(course=course, student=student)
+        evaluation.save()
+        data = {"student_id": "17373456", "bid": "111", "actor": "17373010"}
+        response = client.post("/timetable/evaluation/student/up/", content_type='application/json', data=data)
+        self.assertEqual(response.status_code, 201)
+
+    def test_evaluation_cancel_up_action(self):
+        client = Client()
+        actor = Student(id='17373010', usr_name='111')
+        actor.save()
+        student = Student(id='17373456')
+        student.save()
+        course = Course(bid='111')
+        course.save()
+        evaluation = CourseEvaluation(course=course, student=student)
+        evaluation.save()
+        up_record = EvaluationUpRecord(evaluation=evaluation, student=actor)
+        up_record.save()
+        data = {"student_id": "17373456", "bid": "111", "actor": "17373010"}
+        response = client.post("/timetable/evaluation/student/cancel_up/", content_type='application/json', data=data)
+        self.assertEqual(response.status_code, 201)
+
+    def test_evaluation_down_action(self):
+        client = Client()
+        student = Student(id='17373010', usr_name='111')
+        student.save()
+        student = Student(id='17373456')
+        student.save()
+        course = Course(bid='111')
+        course.save()
+        evaluation = CourseEvaluation(course=course, student=student)
+        evaluation.save()
+        data = {"student_id": "17373456", "bid": "111", "actor": "17373010"}
+        response = client.post("/timetable/evaluation/student/down/", content_type='application/json', data=data)
+        self.assertEqual(response.status_code, 201)
+
+    def test_evaluation_cancel_down_action(self):
+        client = Client()
+        actor = Student(id='17373010', usr_name='111')
+        actor.save()
+        student = Student(id='17373456')
+        student.save()
+        course = Course(bid='111')
+        course.save()
+        evaluation = CourseEvaluation(course=course, student=student)
+        evaluation.save()
+        up_record = EvaluationDownRecord(evaluation=evaluation, student=actor)
+        up_record.save()
+        data = {"student_id": "17373456", "bid": "111", "actor": "17373010"}
+        response = client.post("/timetable/evaluation/student/cancel_down/", content_type='application/json', data=data)
+        self.assertEqual(response.status_code, 201)
+
+    def test_evaluation_teacher_up(self):
+        client = Client()
+        actor = Student(id='17373010', usr_name='111')
+        actor.save()
+        course = Course(bid='111', name='计算机网络', credit='2.0', hours='32', department='计算机学院', type='核心专业类')
+        course.save()
+        teacher = Teacher(name='张辉')
+        teacher.save()
+        add_teacher_relation(teacher, course)
+        data = {"teacher": "张辉", "bid": "111", "actor": "17373010", "action": "up"}
+        response = client.post("/timetable/evaluation/teacher/", content_type='application/json', data=data)
+        self.assertEqual(response.status_code, 201)
+
+    def test_evaluation_teacher_cancel_up(self):
+        client = Client()
+        actor = Student(id='17373010', usr_name='111')
+        actor.save()
+        course = Course(bid='111', name='计算机网络', credit='2.0', hours='32', department='计算机学院', type='核心专业类')
+        course.save()
+        teacher = Teacher(name='张辉')
+        teacher.save()
+        add_teacher_relation(teacher, course)
+        teacher_course = TeacherCourse.objects.get(teacher_id=teacher, course_id=course)
+        up_teacher = TeacherEvaluationRecord(teacher_course=teacher_course, student=actor)
+        up_teacher.save()
+        data = {"teacher": "张辉", "bid": "111", "actor": "17373010", "action": "cancel_up"}
+        response = client.post("/timetable/evaluation/teacher/", content_type='application/json', data=data)
+        self.assertEqual(response.status_code, 201)
